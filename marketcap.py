c92330798a45ff42f606aaf380e4e6a8858151fc
verb55 <verb55nta@gmail.com>
2018-01-02 11:26:54 +0900

    print the volatility: volume/market_cap

--- marketcap.py
+++ marketcap.py
@@ -16,42 +16,32 @@ def scraping(soup):
     for a in soup.find_all('tr'):
         if(a.get('id') is not None):
             for b in a.find_all('td'):
-                #print(b)
                 if(b.get('class') is not None):
                     for c in b.get('class'): # c is class
-                        #print(c)
                         if(re.match("currency-name",c)):
                             for c_name in b.select("a"):
-                                #print(c_name)
                                 if(c_name.get("class") is not None):
-                                    #print(c_name.text) # currency name
                                     currency_name.append(c_name.text)
                         elif(re.match("market-cap",c)):
-                            #print(b.get('data-usd').strip()) #market capacity
                             market_cap.append(b.get('data-usd').strip())
                         elif(re.match("percent",c)):
-                            #print(b.get('data-usd').strip()) # 24 h percent
                             change.append(b.get('data-usd').strip())
                         elif(re.match("circula",c)):
                             for temp_supply in b.select("a"):
                                 if(temp_supply.get("data-supply") is not None):
-                                    #print(temp_supply.get("data-supply")) #currency supply
+
                                     supply.append(temp_supply.get("data-supply"))
-                                #if(c_name.get("class") is not None):
                 for other in b.select("a"):
-                    #print(other)
                     if(other.get("class") is not None):
                         for c2 in other.get('class'): # c is class                 
                             if(re.match("price",c2)):
-                                #print(other.get('data-usd').strip()) #price
                                 price_usd.append(other.get('data-usd').strip())
                             elif(re.match("volume",c2)):
-                                #print(other.get('data-usd').strip()) #volume
                                 volume_usd.append(other.get('data-usd').strip())
-                    #if(re.match("price",other.get("class"))):
 
 
-for i in range(1,11,1):
+
+for i in range(1,21,1):
     #print(i)
     base_url = 'https://coinmarketcap.com/'
     target_url = base_url + str(i)
@@ -59,17 +49,41 @@ for i in range(1,11,1):
     tmp_soup = BeautifulSoup(r.text,'lxml')
     scraping(tmp_soup)
 
-#print(len(currency_name))
-#print(len(price_usd))
-#print(len(volume_usd))
-#print(len(market_cap))
-#print(len(change))
+#print("debug")
+#print(len(currency_name),len(price_usd),len(volume_usd),len(market_cap),len(change))
 
+volatility={}
+
+def is_float_str(num_str, default=0):
+    try:
+        return {"is_float": True ,"val": float(num_str)}
+    except ValueError:
+        return {"is_float": False , "val": default}
+
+for i in range(0,len(currency_name),1):
+    if( is_float_str(volume_usd[i])["is_float"] and is_float_str(market_cap[i])["is_float"] ):
+        volatility[currency_name[i]]=(float(volume_usd[i]) / float(market_cap[i]))
+    else:
+        volatility[currency_name[i]]=0
+
+num=0
+
+for k,v in sorted( volatility.items(),key=lambda x:-x[1]  ):
+
+    #if(float(market_cap[currency_name.index(k)]) < 1e8 ):
+    if(v < 0.1): break
+    if(float(market_cap[currency_name.index(k)]) < 1e7 ):    
+        #print( str(currency_name.index(k)) + ":" + str( float(market_cap[currency_name.index(k)])  )  + ":" + str(k) + ":" + str(v)  )
+        print( str(currency_name.index(k)) + "\t\t" + str( float(market_cap[currency_name.index(k)])  )  + "\t\t" + str(k) + "\t\t" + str(v)  )
+        num+=1
+
+#print("num:"+ str(num))
+        
 #print("currency_name[i],market_cap[i],price_usd[i],volume_usd[i],change[i]")
-print("currency_name[i],market_cap[i],price_usd[i],volume_usd[i]")
+#print("currency_name[i],market_cap[i],price_usd[i],volume_usd[i]")
 
-for i in range(0,1000,1):
+#for i in range(0,1000,1):
     #print(currency_name[i],market_cap[i],price_usd[i],volume_usd[i],change[i])
     #print(currency_name[i],",",market_cap[i],",",price_usd[i],",",volume_usd[i],",",change[i])
-    print(currency_name[i],",",market_cap[i],",",price_usd[i],",",volume_usd[i])
+    #print(currency_name[i],",",market_cap[i],",",price_usd[i],",",volume_usd[i])
 #print(len(supply))
